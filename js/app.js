50c869dce00b9617e77533879a57f1918b018757
Linda Vadysirisack <linda.vady@gmail.com>
2023-05-23 11:30:00 -0500

    controlflow

---
+++ js/app.js
@@ -0,0 +1,35 @@
+let num = 0;
+if (num === +-1) 
+{
+    console.log(num);
+}
+
+//grant access to 18 and older. Deny 18 and under
+let users = 18; 
+
+if (users >= 18) {
+    console.log("Access Granted")
+}
+else if (users < 18) {
+    console.log("Access Denied")
+}
+
+
+//show grades 
+let learner = 54;
+
+if (learner >= 90) {
+    console.log("A");
+}
+else if (learner >= 80) {
+    console.log("B");
+}
+else if (learner >= 70) {
+    console.log("C");
+}
+else if (learner >= 55) {
+    console.log("D");
+}
+else {
+    console.log("F");
+}
