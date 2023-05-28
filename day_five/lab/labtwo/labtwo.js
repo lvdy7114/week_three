011dd6267efa0f52ba6d1ff7f188cf148f5d54b3
Linda Vadysirisack <linda.vady@gmail.com>
2023-05-27 21:05:28 -0500

    Fizz Buzz answered

--- day_five/lab/labtwo/labtwo.js
+++ day_five/lab/labtwo/labtwo.js
@@ -15,3 +15,23 @@ for(let i = 0; i <= 200; i++) {
         console.log(i);
     }
 }
+
+/*
+FizzBuzz
+*/
+//fizzbuzz problem from 1 - 100
+for (let i = 1; i <= 100; i++ ) {
+    if(i % 3 === 0 && i % 5 === 0) {
+        console.log("FizzBuzz");
+    }
+    else if (i % 3 === 0) {
+        console.log("Fizz");
+
+    }
+    else if ( i % 5 === 0 ) {
+        console.log("Buzz");
+    }
+    else {
+        console.log(i);
+    }
+}
