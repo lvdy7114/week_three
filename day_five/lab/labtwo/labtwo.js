7da60ba9fa852ab7c1e0abe0b8a6aecd113f97e3
Linda Vadysirisack <linda.vady@gmail.com>
2023-05-28 11:24:02 -0500

    Wild Wild Life Answered

--- day_five/lab/labtwo/labtwo.js
+++ day_five/lab/labtwo/labtwo.js
@@ -35,3 +35,28 @@ for (let i = 1; i <= 100; i++ ) {
         console.log(i);
     }
 }
+
+/*
+Wild Wild Life
+*/
+//modify elements in arrays - array order: (name, species, age, hometown)
+//1. Plantee just had her birthday; change Plantee's array to reflect her being a year older. 
+const plantee = ["Plantee", "plant", 5000, "Mordor"];
+plantee.splice(2,1, '5001');
+console.log(plantee);
+
+//2. Change Wolfy's hometown from "Yukon Territory" to "Gotham City".
+const wolfy = ["Wolfy", "wolf", 16, "Yukon Territory"];
+wolfy.splice(3,1, 'Gotham City');
+console.log(wolfy);
+
+//3. Give D'Art a second hometown by adding "Hawkins"
+const dart = ["D'Art" , "Demogorgan Dog", 2, "Upside Down"];
+dart.push('Hawkins');
+console.log(dart);
+
+//4. Porgee decides that Wolfy can't be named "Wolfy" anymore. 
+//Remove "Wolfy" from the wolfy array and replace it with "Gameboy".
+wolfy.splice(0,1, 'Gameboy');
+console.log(wolfy);
+
