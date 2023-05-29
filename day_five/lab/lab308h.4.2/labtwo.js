22e856f8be7714f3ca8acdbafb96ba39abb788f5
Linda Vadysirisack <linda.vady@gmail.com>
2023-05-28 22:35:42 -0500

    updated directory

--- day_five/lab/lab308h.4.2/labtwo.js
+++
@@ -1,7 +0,0 @@
-/*
-Easy Going 
-*/
-//write a loop that will log the numbers 1 through 20. 
-for (let i = 1; i <= 20; i ++) {
-    console.log(i);
-}
--- day_five/lab/labtwo/labtwo.js
+++ day_five/lab/labtwo/labtwo.js
@@ -239,3 +239,38 @@ const findMedian = Math.floor(nums.length / 2);
 const midPoint = nums[findMedian];
 console.log(midPoint);
 
+/*
+Hungry For More - optional
+*/
+//alien attire - Remove Kristyn's shoe from the array and save it to the variable kristynsShoe.
+//Use that variable to add Kristyn's lost shoe to Thom's accessories array.
+const kristynsCloset = [
+    "left shoe",
+    "cowboy boots",
+    "right sock",
+    "Per Scholas hoodie",
+    "green pants",
+    "yellow knit hat",
+    "marshmallow peeps"
+  ];
+  
+  // Thom's closet is more complicated. Check out this nested data structure!!
+  const thomsCloset = [
+    [
+      // These are Thom's shirts
+      "grey button-up",
+      "dark grey button-up",
+      "light blue button-up",
+      "blue button-up",
+    ],[
+      // These are Thom's pants
+      "grey jeans",
+      "jeans",
+      "PJs"
+    ],[
+      // Thom's accessories
+      "wool mittens",
+      "wool scarf",
+      "raybans"
+    ]
+  ];
